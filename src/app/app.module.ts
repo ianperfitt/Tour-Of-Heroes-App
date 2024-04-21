21c269aeb962f44a844e94e2891bcbf245772108
Ian Perfitt <iancp@protonnmail.com>
2024-04-21 17:45:47 -0500

    implemented list display functionality in template and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgFor
   ],
   providers: [],
   bootstrap: [AppComponent]
